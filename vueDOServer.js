30b46fd12f05e5d1503228670389067bad820e46
Christian Moser <christianmoser88@gmail.com>
2017-07-30 18:09:52 +0200

    update

--- vueDOServer.js
+++ vueDOServer.js
@@ -4,8 +4,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, 'localhost');
-/*app.get('/',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join('/index.html'));
   //__dirname : It will resolve to your project folder.
-});*/
+});
 console.log('Server running at http://localhost:8080/');
